eaefd4d8dc978ccb6c28afc92be44b4682d52ae9
Murali <muralikrishnay.479@gmail.com>
2024-11-27 20:13:24 +0530

    first commit

--- accounts/utils/notifications.py
+++ accounts/utils/notifications.py
@@ -1,2 +1,15 @@
+from twilio.rest import Client
+from django.conf import settings
 
+def send_sms(to, message):
+    account_sid = settings.ACCOUNT_SID
+    auth_token = settings.AUTH_TOKEN   
+    client = Client(account_sid, auth_token)
+
+    client.messages.create(
+        to=to,
+        from_='+12564084041',  
+        body=message
+    )
+        
 
